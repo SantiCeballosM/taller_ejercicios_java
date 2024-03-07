10cbce3b1485aeda47324864891acfd5486d84ce
SantiCeballosM <107371068+SantiCeballosM@users.noreply.github.com>
2024-03-06 22:42:05 -0500

    Sexta chamba

--- src/ejercicio_5_1.java
+++ src/ejercicio_5_1.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class ejercicio_5_1 {
-}
+import java.util.Scanner;
+
+public class ejercicio_5_1 {
+    /*Ejercicio de Suma de Dígitos:
+     Escribe un programa en Java que solicite al usuario ingresar un número entero positivo.
+
+     Luego, suma todos los dígitos del número y muestra el resultado.
+     Ejemplo: Usuario ingresa 12 → El programa retorna 3 SumDigitos = 1+2= 3*/
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        try{
+            System.out.println("Ingresa un numero positivo: ");
+            int num = scanner.nextInt();
+            int sum = 0;
+            int numI = num;
+            while(num > 0){
+                sum += num %10;
+                num /= 10;
+            }
+            System.out.println("La suma de los digitos de "+numI+ " es "+sum);
+        } catch (Exception e) {
+            System.out.println("Ups parce hiciste algo mal");
+        }
+    }
+    }
