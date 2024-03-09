b3a7df6402aa96eb1c685462a0d85c61f5c2f111
SantiCeballosM <107371068+SantiCeballosM@users.noreply.github.com>
2024-03-08 19:48:11 -0500

    ejem

---
+++ src/ejercicio_14.java
@@ -0,0 +1,12 @@
+/*14. Eliminar duplicados:
+Escribir un programa que elimine los elementos duplicados de una lista.*/
+public class ejercicio_14 {
+    public static void main(String[] args){
+        try{
+            int list=[];
+
+        } catch (Exception e) {
+            System.out.println("Uy parce hiciste algo mal");
+        }
+    }
+}
